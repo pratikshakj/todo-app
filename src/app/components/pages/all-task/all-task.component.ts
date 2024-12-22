2721abb75fba416cf579bd2830a312007479a779
Z8PKJ <pkj@kmd.dk>
2024-12-22 16:13:02 +0530

    Db configuration and Add of task through application and HTTP services

--- src/app/components/pages/all-task/all-task.component.ts
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpService } from './../../../services/http.service';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './all-task.component.html',
   styleUrl: './all-task.component.scss'
 })
 export class AllTaskComponent {
+  newTask='';
+  taskList:any []=[];
+  httpService = inject(HttpService);
 
+  ngOnInit(): void {
+    this.getALlTask();
+  }
+
+  addTask(){
+    console.log(this.newTask,"TaskAdded");
+    this.httpService.addTask(this.newTask).subscribe(()=>{
+    this.newTask ='';
+    this.getALlTask();
+    })
+  }
+
+  getALlTask(){
+    this.httpService.getALlTask().subscribe((result:any)=>{
+      this.taskList=result;
+    })
+  }
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root' // makes the service avaliable application wide
+})
+export class HttpService {
+
+  constructor() { }
+  httpClient = inject(HttpClient);
+
+  addTask(task:string){
+    return this.httpClient.post("http://localhost:3000/tasks",{   //post request method
+      title:task
+    });
+  }
+
+  getALlTask(){
+    return this.httpClient.get("http://localhost:3000/tasks"); //get request method
+  }
+}
+
