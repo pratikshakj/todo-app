69f179f2e2123c4b2613162b31e4b8ae3939cb5b
Z8PKJ <pkj@kmd.dk>
2025-01-06 19:37:10 +0530

    Completed task in completed section, important task in important section

--- src/app/components/pages/completed-task/completed-task.component.ts
+++ src/app/components/pages/completed-task/completed-task.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PageTitleComponent } from '../../page-title/page-title.component';
+import { TaskListsComponent } from '../../task-lists/task-lists.component';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-completed-task',
   standalone: true,
-  imports: [],
+  imports: [PageTitleComponent,TaskListsComponent],
   templateUrl: './completed-task.component.html',
   styleUrl: './completed-task.component.scss'
 })
 export class CompletedTaskComponent {
+    newTask = '';
+    taskList: any[] = [];
+    httpService = inject(HttpService);
+
+    ngOnInit(): void {
+      this.getALlTask();
+    }
+
+    getALlTask() {
+      this.httpService.getALlTask().subscribe((result: any) => {
+        this.taskList = result.filter((comp:any)=>comp.completed==true);
+      });
+    }
+
+    onComplete(task:any){
+      task.completed = true;
+      console.log('Task Completed',task);
+      this.httpService.updateTask(task).subscribe(() =>{
+      })
+    }
 
 }
--- src/app/components/pages/important-task/important-task.component.ts
+++ src/app/components/pages/important-task/important-task.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PageTitleComponent } from '../../page-title/page-title.component';
+import { TaskListsComponent } from '../../task-lists/task-lists.component';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-important-task',
   standalone: true,
-  imports: [],
+  imports: [PageTitleComponent, TaskListsComponent],
   templateUrl: './important-task.component.html',
   styleUrl: './important-task.component.scss'
 })
 export class ImportantTaskComponent {
+  newTask = '';
+  taskList: any[] = [];
+  httpService = inject(HttpService);
 
+  ngOnInit(): void {
+    this.getALlTask();
+  }
+
+  getALlTask() {
+    this.httpService.getALlTask().subscribe((result: any) => {
+      this.taskList = result.filter((imp:any)=>imp.important==true);
+    });
+  }
+
+  onImportant(task:any){
+    task.important = true;
+    console.log('Task Important',task);
+    this.httpService.updateTask(task).subscribe(()=>{
+    })
+  }
 }
