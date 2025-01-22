35702413f0e5ded162022bc0f8d8423488d5e395
Z8PKJ <pkj@kmd.dk>
2025-01-22 18:58:40 +0530

    Changes for the completed task list

--- src/app/components/pages/completed-task/completed-task.component.ts
+++ src/app/components/pages/completed-task/completed-task.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { PageTitleComponent } from '../../page-title/page-title.component';
 import { TaskListsComponent } from '../../task-lists/task-lists.component';
 import { HttpService } from '../../../services/http.service';
+import { StateService } from '../../../services/state.service';
 
 @Component({
   selector: 'app-completed-task',
@@ -13,15 +14,26 @@ import { HttpService } from '../../../services/http.service';
 export class CompletedTaskComponent {
     newTask = '';
     taskList: any[] = [];
+    InitialTaskList: any[] = [];
     httpService = inject(HttpService);
+    stateService =inject(StateService);
 
     ngOnInit(): void {
       this.getALlTask();
+      this.stateService.searchSubject.subscribe((value) => {
+        if(value){
+          this.taskList =this.InitialTaskList.filter((x) =>{
+            return x.title.toLowerCase().includes(value.toLowerCase())
+          })
+        }
+      });
+      this.getALlTask
     }
 
     getALlTask() {
       this.httpService.getALlTask().subscribe((result: any) => {
         this.taskList = result.filter((comp:any)=>comp.completed==true);
+        this.InitialTaskList = this.taskList = result.filter((comp:any)=>comp.completed==true);
       });
     }
 
